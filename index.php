d14ed756cfce3dbdf38416b9259584309f5b9349
Collective Cloud Peru <collectivecloudperu@gmail.com>
2015-08-16 23:47:20 -0500

    Unicode Class CSS 3
    Aplicar Propiedades CSS a los Caracteres Unicodes

---
+++ index.php
@@ -0,0 +1,107 @@
+﻿<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+    <title>Unicode Class en CSS 3</title>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">    
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+    <link rel="stylesheet" type="text/css" href="css/estilos.css">
+
+  </head>
+  <body>
+    <div class="container">
+
+      <h1 align="center">Unicode Class en CSS 3</h1>
+
+      <div class="row" align="center">
+        
+
+        <div class="col-md-4">
+
+          <br><br>
+
+          <p>
+            font-size: 20px;<br> 
+            color: orange;           
+          </p>
+
+          <p class="ಠ_ಠ">ಠ_ಠ</p>
+
+          <p class="❤">❤</p>
+
+          <p class="☺">☺</p>
+
+          <p class="✌">✌</p>
+          
+        </div>
+
+
+        <div class="col-md-4">
+
+          <br><br>
+
+          <p>
+            font-size: 40px;<br>
+            color: purple;          
+          </p>
+
+          <p class="☀">☀</p>
+
+          <p class="♫">♫</p>
+
+          <p class="☃">☃</p>
+
+          <p class="☂">☂</p>
+          
+        </div>
+
+
+        <div class="col-md-4">
+
+          <br><br>
+
+          <p>
+            font-size: 60px;<br>
+            color: green;         
+          </p>
+
+          <p class="★">★</p>
+
+          <p class="✂">✂</p>
+
+          <p class="✈">✈</p>
+
+          <p class="✏">✏</p>
+          
+        </div>
+
+      </div>
+
+      <p>Puedes seleccionar otros caracteres en: <a href="http://apps.timwhitlock.info/emoji/tables/unicode">www.apps.timwhitlock.info/emoji/tables/unicode</a></p>
+
+    </div>
+
+    <br>
+
+  <footer>
+
+      <div class="container text-center">
+
+        <p class="text-muted credit">
+
+          Desarrollado por <a href="http://www.collectivecloudperu.com" target="blank">Collective Cloud Peru</a>
+
+        </p>
+
+      </div>        
+
+    </footer>    
+
+  </body>
+</html>
